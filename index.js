a8a9df3c6c33b169326f296de73289091799327c
Avinash Kumawat <avinash@orderlink.nz>
2026-03-14 12:11:40 +0530

    improve - link social icon - add faq button to link faq component on home page - remove blogs button from header - fix css of header and footer for company name

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ function goToPage(page) {
     window.location.href = page;
   }
 }
+
+function scrollToFooter() {
+  const footer = document.querySelector(".footer-upper");
+  if (footer) {
+    footer.scrollIntoView({ behavior: "smooth" });
+  }
+}
